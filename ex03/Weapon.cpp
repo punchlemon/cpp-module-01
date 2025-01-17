8178eebfcb17a597fd6c6b3daf55ecb9080ae47f
punchlemon <tsukiwahigashini0410@gmail.com>
2025-01-17 13:00:57 +0900

    update weapon.cpp

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -9,9 +9,7 @@ void Weapon::setType(const std::string& type) {
     _type = type;
 }
 
-Weapon::Weapon(const std::string& type) {
-    _type = type;
-}
+Weapon::Weapon(const std::string& type) : _type(type) {}
 
 Weapon::~Weapon() {
     std::cout
