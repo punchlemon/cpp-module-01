65f114f8b9e3aeaacf6cf5a5b29909714f9a40c5
punchlemon <tsukiwahigashini0410@gmail.com>
2025-01-19 17:12:45 +0900

    update sentence

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -4,38 +4,35 @@
 
 void Harl::_debug() {
     std::cout
-        << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << ' '
+        << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl
         << "I really do!" << std::endl;
 }
 
 void Harl::_info() {
     std::cout
-        << "I cannot believe adding extra bacon costs more money." << ' '
-        << "You didn’t put enough bacon in my burger!" << ' '
+        << "I cannot believe adding extra bacon costs more money." << std::endl
+        << "You didn’t put enough bacon in my burger!" << std::endl
         << "If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::_warning() {
     std::cout
-        << "I think I deserve to have some extra bacon for free." << ' '
+        << "I think I deserve to have some extra bacon for free." << std::endl
         << "I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::_error() {
     std::cout
-        << "This is unacceptable!" << ' '
-        << "I want to speak to the manager now." << std::endl;
+        << "This is unacceptable, I want to speak to the manager now." << std::endl;
 }
 
 void Harl::complain(const std::string& level) {
-    std::map<std::string, void (Harl::*)()>::iterator it;
-    if (!level.empty() && (it = _commands.find(level)) != _commands.end()) {
+    std::map<std::string, void (Harl::*)()>::iterator it = _commands.find(level);
+    if (it != _commands.end()) {
         std::cout << "[ " << level << " ]" << std::endl;
         (this->*(it->second))();
-    } else {
-        std::cout << "[ Probably complaining about insignificant problems ]";
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
 }
 
 Harl::Harl() {
